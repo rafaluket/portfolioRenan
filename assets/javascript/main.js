77046a67af6554177da9056a76c7c15b13eaf172
Rafael Luquete <rafael.luquete@gmail.com>
2022-02-08 22:10:36 -0300

    Finished Project

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,27 +1,57 @@
 $(document).ready(function () {
 
-    $('.skillsCarousel').slick({
-        infinite: true,
-        slidesToShow: 3,
-        slidesToScroll: 3,
-        dots: true,
-        responsive: [
-            {
-              breakpoint: 768,
-              settings: {
-                arrows: false,
-                slidesToShow: 1,
-                slidesToScroll: 1,
-              }
-            },
-          ]
-      });
+  $('.skillsCarousel').slick({
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    dots: true,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          arrows: false,
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        }
+      },
+    ]
+  });
+
+  document.getElementById('formEmail')
+    .addEventListener('submit', function (event) {
+      event.preventDefault();
+
+      const btn = document.getElementById('button').value = "Sending...";
+
+      const serviceID = 'service_';
+      const templateID = 'template_';
+
+      emailjs.sendForm(serviceID, templateID, this)
+        .then(() => {
+          btn.value = btnFormValue;
+          Swal.fire({
+            title: "Success!",
+            text: "E-mail sent",
+            icon: 'success',
+            confirmButtonText: 'OK'
+          })
+        }, (err) => {
+          btn.value = btnFormValue;
+          Swal.fire({
+            title: "Error!",
+            text: "Error: " + JSON.stringify(err),
+            icon: 'error',
+            confirmButtonText: 'OK'
+          })
+        });
+    }
+    );
 });
 
 $(function () {
-    $(document).scroll(function () {
-      var $nav = $(".navbar.bg-light");
-      $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
-      $nav.toggleClass('mt-5', $(this).scrollTop() < $nav.height());
-    });
+  $(document).scroll(function () {
+    var $nav = $(".navbar.bg-light");
+    $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
+    $nav.toggleClass('mt-5', $(this).scrollTop() < $nav.height());
   });
+});
